66970e07c712526699726b7230ac0c232a6ea42b
rkdrud34 <rkdrud34@nate.com>
2022-02-04 06:37:36 +0900

    DB접근2. jdbctemplate

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,6 +1,7 @@
 package hello.hellospring;
 
 import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.sevice.MemberService;
 import org.springframework.context.annotation.Bean;
@@ -29,7 +30,10 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
+
+
         // 객체지향설계의 장점을 보여줌(다형성) -> assembly code(조립하느코드) 이것만 바꾸면 다른 구현체를 끼워넣기만 하면 사용가능
         // 개방 폐쇄원칙(OCP, open-closed principle) 확장에느 ㄴ열려있고 수정/변경에는 닫혀있다.
         // interface 에서 구현체를 바꾸면서 기존 코드는 건들지 않으면서 기능 확장/추가가 가능하다는것이 객체지향의 꽃
