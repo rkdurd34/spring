a33fd0bef64a92ac30b60e770456d790ec951e02
rkdrud34 <rkdrud34@nate.com>
2022-02-09 23:55:35 +0900

    순수자바 테스트 코드 작성

---
+++ core/src/main/java/helllo/core/MemberApp.java
@@ -0,0 +1,21 @@
+package helllo.core;
+
+import helllo.core.member.Grade;
+import helllo.core.member.Member;
+import helllo.core.member.MemberService;
+import helllo.core.member.MemberServiceImpl;
+
+public class MemberApp {
+    // 순수 자바로 테스트 하는 방법
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        // soutv 하면 위의 변수들을 자동으로 가져올 수 있음!!
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+
+    }
+}
