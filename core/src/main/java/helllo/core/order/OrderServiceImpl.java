7e62b58d6861871cec7b87f4d652e0bcfb47d7bb
rkdrud34 <rkdrud34@nate.com>
2022-02-13 22:15:10 +0900

    DIP위반 상태

--- core/src/main/java/helllo/core/order/OrderServiceImpl.java
+++ core/src/main/java/helllo/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package helllo.core.order;
 
 import helllo.core.discount.DiscountPolicy;
 import helllo.core.discount.FixDiscountPolicy;
+import helllo.core.discount.RateDiscountPolicy;
 import helllo.core.member.Member;
 import helllo.core.member.MemberRepository;
 import helllo.core.member.MemoryMemberRepository;
@@ -9,7 +10,12 @@ import helllo.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // DIP위반 discount policy interface에 의존해야대는데 구현체에도 지금 의존중..-> OCP위반
+    // FixdiscountPolicy를 했는데 -> RateDiscountPolicy로 직접 바꿔주어야댐
+    // 추상에만 의존해야댐!!!!!!!!!
+    // 클라이언트(OrderServiceImpl)에 영향을 준다..
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
