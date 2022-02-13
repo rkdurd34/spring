aa012d2121b2375e036d6c6b46c032ffdae15d6f
rkdrud34 <rkdrud34@nate.com>
2022-02-13 22:58:31 +0900

    AppConfig 리팩토링을 통한 중복 제거

--- core/src/main/java/helllo/core/AppConfig.java
+++ core/src/main/java/helllo/core/AppConfig.java
@@ -1,6 +1,8 @@
 package helllo.core;
 
+import helllo.core.discount.DiscountPolicy;
 import helllo.core.discount.RateDiscountPolicy;
+import helllo.core.member.MemberRepository;
 import helllo.core.member.MemberService;
 import helllo.core.member.MemberServiceImpl;
 import helllo.core.member.MemoryMemberRepository;
@@ -12,13 +14,21 @@ public class AppConfig {
     // MemberService 객체는 MemoryMemberreposiroy "구현체"랑 연기를 하렴
     // MemberService 구현체에서는 오로지 memberRepository "인터페이스"와 의존
     // 주입은 "공연기획자"인 Appconfig에서 하기
+
+    //이렇게 분리하게 되면 discountPolicy 변경시 한곳만 변경하여 다른 곳에 자동으로 주입 할 수 있다.
+    // 역할(선언)과 구현(리턴) 클래스가 구분되어 한눈에 보임
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService(){
-
-        return new OrderServiceImpl(new RateDiscountPolicy(), new MemoryMemberRepository());
+        return new OrderServiceImpl(discountPolicy(), memberRepository());
+    }
+    public DiscountPolicy discountPolicy(){
+        return new RateDiscountPolicy();
+    }
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
     }
 
 }
