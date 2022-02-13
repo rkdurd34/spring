fbcb1c6045a5ed2d67cabaad22b71de0db2787b1
rkdrud34 <rkdrud34@nate.com>
2022-02-14 00:39:28 +0900

    컨테이너안의 빈을 통한 객체 호출

--- core/src/main/java/helllo/core/AppConfig.java
+++ core/src/main/java/helllo/core/AppConfig.java
@@ -9,10 +9,14 @@ import helllo.core.member.MemberServiceImpl;
 import helllo.core.member.MemoryMemberRepository;
 import helllo.core.order.OrderService;
 import helllo.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 // 공연 기획자 ->"어떤" 배우들끼리 연기를 하게 될지 정하느 "기획서"
 
-// IoC 컨테이너, DI 컨테이너 -> 제어의 역전 / 동적인 객체 인스턴스 의존관계 를 변경
+// IoC 컨테이너, DI 컨테이너 -> 동적인 객체 인스턴스 의존관계 를 변경
+// Ioc(제어의 역전) -> 객체에 대한 제어권이 컨테이너(스프링)로 역전 되는 상황 (생명주기 관리 등등)
 // 어셈블러라고도 불림 -> 어떤 객체들을 의존 관계로 둘지 설정해주면서 <조립> 하기 떄문에
+@Configuration
 public class AppConfig {
 
     // MemberService 객체는 MemoryMemberreposiroy "구현체"랑 연기를 하렴
@@ -21,17 +25,20 @@ public class AppConfig {
 
     //이렇게 분리하게 되면 discountPolicy 변경시 한곳만 변경하여 다른 곳에 자동으로 주입 할    있다.
     // 역할(선언)과 구현(리턴) 클래스가 구분되어 한눈에 보임
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(discountPolicy(), memberRepository());
     }
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
 //        return new FixDiscountPolicy();
     }
+    @Bean
     public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
--- core/src/main/java/helllo/core/MemberApp.java
+++ core/src/main/java/helllo/core/MemberApp.java
@@ -4,12 +4,21 @@ import helllo.core.member.Grade;
 import helllo.core.member.Member;
 import helllo.core.member.MemberService;
 import helllo.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     // 순수 자바로 테스트 하는 방법
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+
+        //  스프링은 모두 여기서 시작됨 == 스프링 컨테이너라고 보면 됨 -> @bean을 모두 고나리
+        // @bean 생성한것을 컨테이너안에 다 넣어줌
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
